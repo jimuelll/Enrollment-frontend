6c3f19fb125aa9e88bf10f506688934adab4636d
Jimuel Untalan <131651496+jimuelll@users.noreply.github.com>
2026-03-06 13:18:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const content = document.querySelector("#content");
+const submit = document.querySelector("#add");
+window.addEventListener("load", () => {
+  getUsers();
+});
+
+function getUsers() {
+  let html = "";
+  //FETCH API
+  fetch("https://enrollment-ylkk.onrender.com/api/enrollments", {
+    mode: "cors",
+  })
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        html += `<li>$${element.id} - ${element.fullName} - ${element.course} - ${element.yearLevel} - ${element.email} - ${element.createdAt}</li>`;
+      });
+      content.innerHTML = html;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+//POST API
+submit.addEventListener("click", () => {
+  const student = {
+    fullName: document.querySelector("#fullName").value,
+    course: document.querySelector("#course").value,
+    yearLevel: document.querySelector("#yearLevel").value,
+    email: document.querySelector("#email").value,
+  };
+  fetch("https://enrollment-ylkk.onrender.com/api/enrollments", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(student),
+  }).catch((error) => {
+    console.log(error);
+  });
+  alert("Student enrolled successfully");
+  location.reload();
+});
